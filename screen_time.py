602651a70b1cc3c6a3e27a43ed62896f0f667fbd
chaudhary-dotcom <oends38@gmail.com>
2025-05-13 22:54:02 +0545

    Tips Dataset

---
+++ screen_time.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
